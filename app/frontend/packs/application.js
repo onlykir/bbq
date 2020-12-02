b77ff93447106f26cc8d2695a89822cfbb6e5632
onlykir <kekelev.k@mail.ru>
2020-12-02 13:02:10 +0300

    Add bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,6 +2,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 
 import '../styles/application'
+import 'bootstrap/dist/js/bootstrap'
 
 const images = require.context('../images', true)
 
