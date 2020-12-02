01fb4d8118d07650325ef8862d28e40deedd72b5
onlykir <kekelev.k@mail.ru>
2020-12-02 14:46:17 +0300

    add migration user_id to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 255},
+            uniqueness: true, format: {with: EMAIL_VALID}
 end
