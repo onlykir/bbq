d64c4bdcac8e2889685e2e08a0f20dc80d21622d
onlykir <kekelev.k@mail.ru>
2020-12-06 02:47:54 +0300

    add ability to change language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :danger, :info, :warning, :success
@@ -7,5 +8,19 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.enum_for(:account_update) do |u|
       u.permit(:password, :password_confirmation, :current_password)
     end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
+      parsed_locale.to_sym : nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'events#index'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
+    root 'events#index'
 
-  resources :events
-  resources :users, only: [:show, :edit, :update]
+    resources :events
+    resources :users, only: [:show, :edit, :update]
+  end
 end
