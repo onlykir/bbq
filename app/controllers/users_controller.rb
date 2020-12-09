2f127d669d14442f579bf298d00c0293b50f16fd
onlykir <kekelev.k@mail.ru>
2020-12-09 19:32:47 +0300

    translate devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: I18n.t('controllers.users.destroyed')
+  end
+
   private
 
   def set_current_user
