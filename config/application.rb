f651bd0a9a991417b7116441291549a0eabe5f8e
onlykir <kekelev.k@mail.ru>
2020-12-12 02:57:05 +0300

    change Time.zone to Moscow

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Bbq
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.time_zone = 'Moscow'
     # Don't generate system test files.
     config.generators.system_tests = nil
 
