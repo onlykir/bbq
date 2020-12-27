85fe8b5cc1ca86bb0f3cc7ab9616b6f7ba267d45
onlykir <kekelev.k@mail.ru>
2020-12-27 21:22:25 +0300

    Add sendgrid-ruby gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'mini_magick'
 gem 'fog-aws'
 
 gem 'dotenv-rails'
+gem 'sendgrid-ruby'
 
 group :development, :test do
   gem 'pry-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: 'plain',
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'goonbbq.herokuapp.com',
     enable_starttls_auto: true
   }
 
