dd22972e0907ec25eb570dafc01b035cd64f0c12
onlykir <kekelev.k@mail.ru>
2021-01-12 15:48:11 +0300

    author of comment doesn't receive notification

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,7 +46,7 @@ class CommentsController < ApplicationController
 
   def notify_subscribers(event, comment)
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [comment.user.email]
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
