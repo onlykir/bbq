9aafe4743bdfdb3380f6cee2f8d52cfc374ad0f8
onlykir <kekelev.k@mail.ru>
2021-01-14 16:04:24 +0300

    edit senders titles

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: t('mailers.event_mailer.subscription') + event.title
+    mail to: event.user.email, subject: t('mailers.event_mailer.subscription') + '   @' + event.title
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject:  t('mailers.event_mailer.comment') + '@' + event.title
+    mail to: email, subject:  t('mailers.event_mailer.comment') + '   @' + event.title
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject:  t('mailers.event_mailer.photo') + '@' + event.title
+    mail to: email, subject:  t('mailers.event_mailer.photo') + '   @' + event.title
   end
 end
