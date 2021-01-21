635435895dcac3c0efd1fb9646453590f1e8c347
onlykir <kekelev.k@mail.ru>
2021-01-21 11:40:19 +0300

    add lightbox in event

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,7 +2,12 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require('data-confirm-modal')
 
+var jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 import '../styles/application'
 import 'bootstrap/dist/js/bootstrap'
 import '../scripts/map'
+import '../scripts/lightbox'
 const images = require.context('../images', true)
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
 module PhotosHelper
+  def delete_photo?(event, photo)
+    if current_user_can_edit?(event) || current_user == photo.user
+      link_to fa_icon("fas fa-trash"), event_photo_path(@event, photo),
+              method: :delete, data: { title: ' ',
+                                       confirm: t('pages.photos.confirm.consent'),
+                                       commit: t('global.confirm.ok'),
+                                       cancel: t('global.confirm.cancel') }
+    end
+  end
 end
