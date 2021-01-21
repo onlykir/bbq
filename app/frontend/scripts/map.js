0057b104411bc333f86948de4d96be7437e6706b
onlykir <kekelev.k@mail.ru>
2021-01-21 10:06:05 +0300

    fix error in index.html in yandex map

--- app/frontend/scripts/map.js
+++ app/frontend/scripts/map.js
@@ -2,31 +2,38 @@ ymaps.ready(init);
 var myMap;
 
 function init(){
-    address = document.getElementById('map').getAttribute('data-address');
+    map = document.getElementById('map');
+    if (map) {
+        address = map.getAttribute('data-address');
 
-    myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 10
+        });
 
-    myGeocoder = ymaps.geocode(address);
+        myGeocoder = ymaps.geocode(address);
 
-    myGeocoder.then(
-        function (res) {
-            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+        myGeocoder.then(
+            function (res) {
+                coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-            myMap.geoObjects.add(
-                new ymaps.Placemark(
-                    coordinates,
-                    {iconContent: address},
-                    {preset: 'islands#blueStretchyIcon'}
-                )
-            );
+                myMap.geoObjects.add(
+                    new ymaps.Placemark(
+                        coordinates,
+                        {iconContent: address},
+                        {preset: 'islands#blueStretchyIcon'}
+                    )
+                );
 
-            myMap.setCenter(coordinates);
-            myMap.setZoom(15);
-        }, function (err) {
-            alert('Ошибка при определении местоположения');
-        }
-    );
+                myMap.setCenter(coordinates);
+                myMap.setZoom(15);
+            }, function (err) {
+                alert('Ошибка при определении местоположения');
+            }
+        );
+    }
+    else {
+        return;
+    }
+
 }
